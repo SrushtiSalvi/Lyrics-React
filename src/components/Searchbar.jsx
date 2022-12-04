fe747deaaf77a129c3bf5a5a3011f96810978619
Srushti Salvi <srushtisalvi4@gmail.com>
2022-12-05 01:13:56 +0530

    searchbar fixed

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -14,11 +14,11 @@ const Searchbar = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-gray-400 focus-within:text-gray-600">
+    <form onSubmit={handleSubmit} autoComplete="off" className=" p-2 text-gray-400 focus-within:text-gray-600 flex">
       <label htmlFor="search-field" className="sr-only">
         Search all files
       </label>
-      <div className="flex flex-row justify-start items-center w-full">
+      <div className="flex flex-row justify-start items-center">
         <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
         <input
           name="search-field"
